6b89cf76dcf7f7e5a4e29df2856c6c2b8d92d34c
1945258983@qq.com <1945258983@qq.com>
2019-07-27 14:04:23 +0800

    sort
    2019年7月27日14:04:20

--- 6-1.sort/animation/addEventListenerResize.js
+++ 6-1.sort/animation/addEventListenerResize.js
@@ -1,11 +1,13 @@
 let addEventListennerResize = {}
 addEventListennerResize.addEventListennerResize = function () {
-    var divImg = document.querySelector('div>img');
+    var divImg = document.querySelectorAll('div>img');
     if (window.matchMedia('(max-width:430px)').matches) {
-        divImg.setAttribute('width', '90%')
+        for (let i = 0; i < divImg.length; i++)
+            divImg[i].setAttribute('width', '90%');
     } else {
         //当前设备宽度大于300px像素
-        divImg.removeAttribute('width')
+        for (let i = 0; i < divImg.length; i++)
+            divImg[i].setAttribute('width', '');
     }
 }
 window.addEventListener('resize', addEventListennerResize.addEventListennerResize, false);
